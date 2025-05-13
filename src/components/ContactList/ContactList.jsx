94b4f019c586de9ebc16ac9b07e7d544d33996f0
Yura Shaplavsky <yurikorock777@gmail.com>
2025-05-13 18:23:15 +0300

    edit contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,12 @@
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteContact } from "../../redux/contactsSlice";
 
-export default function ContactList() {
-  const dispatch = useDispatch();
-  const contacts = useSelector((state) => state.contacts.items);
-
-  const handleDelete = (id) => {
-    dispatch(deleteContact(id));
-  };
+export default function ContactList({ contacts, onDelete }) {
   return (
     <ul className={css.list}>
       {contacts.map(({ id, name, number }) => (
         <li key={id}>
-          <Contact
-            id={id}
-            name={name}
-            number={number}
-            onDelete={handleDelete}
-          />
+          <Contact id={id} name={name} number={number} onDelete={onDelete} />
         </li>
       ))}
     </ul>
